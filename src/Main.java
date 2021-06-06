8d5ec053e2274edd9c94bd899af06da1c5862f76
thaophamhetxat <thaophamhetxat@gmail.com>
2021-06-06 11:26:05 +0700

    varialbe data type

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float j = 20.5f;
+        double d = 20.5;
+        boolean e = true;
+        char c = 'a';
+        String s = "Ha noi";
+        System.out.println("i = "+ i);
+        System.out.println("j = "+ j);
+        System.out.println("d = "+ d);
+        System.out.println("e = "+ e);
+        System.out.println("c = "+ c);
+        System.out.println("s = "+ s);
+    }
+
+}
+
